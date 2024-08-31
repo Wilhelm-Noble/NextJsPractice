a740e2d89e6970bdba0b4ebed46c58e418b3a262
Wilhelm Noble <100698823+Wilhelm-Noble@users.noreply.github.com>
2024-08-31 23:51:58 +0800

    practice next

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <h1>this element shows at every route in next</h1>
     </html>
   );
 }
